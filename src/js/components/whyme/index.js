39ed57d8d7583cc9c30cc7eb48e6c63676b5ac6f
jayoharedee <jordan@caxap.design>
2019-09-27 21:04:12 -0400

    startwed whyme component

---
+++ src/js/components/whyme/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './style.scss'
+
+function WhyMe() {
+    
+  return (
+    <div className="why-me">
+      <h1>MY PHILOSOPHY</h1>
+      <p>A few thoughts on my respective subject matter expertise</p>
+    </div>
+  )
+}
+
+export default WhyMe
