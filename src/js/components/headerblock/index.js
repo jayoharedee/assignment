7fa3a657fd8f23b1a56937588a9885d1f343b8fa
Jordan Michael <jordanm@tuta.io>
2019-10-05 17:49:24 -0400

    start of headerblock component

---
+++ src/js/components/headerblock/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// import './style.scss'
+
+function HeaderBlock() {
+    
+  return (
+    <div className="header-block">
+      <h1>UX/UI DESIGN</h1>
+      <p>some content here.</p>
+    </div>
+  )
+}
+
+export default HeaderBlock
