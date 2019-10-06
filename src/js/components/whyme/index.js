0a1c4693821650d67da48f6f71734775e58aba2f
Jordan Michael <jordanm@tuta.io>
2019-10-06 14:15:39 -0400

    added parent container and associated styles

--- src/js/components/whyme/index.js
+++ src/js/components/whyme/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import SpinnyIcon from '../spinnyicon'
 import HeaderBlock from '../headerblock'
+
 import './style.scss'
 
 function WhyMe() {
@@ -10,18 +11,20 @@ function WhyMe() {
     <div className="why-me">
       <h1>MY PHILOSOPHY</h1>
       <p>A few thoughts on my respective subject matter expertise</p>
-      <article id="icon-row">
-        <SpinnyIcon icon={'fa fa-laptop'} />
-       <HeaderBlock />
-      </article>
-      <article id="icon-row">
-        <SpinnyIcon icon={'fa fa-laptop'} />
-        <HeaderBlock />
-      </article>
-      <article id="icon-row">
-        <SpinnyIcon icon={'fa fa-laptop'} />
-        <HeaderBlock />
-      </article>
+      <aside id="philosophy-row">
+        <article className="philosophy">
+          <SpinnyIcon icon={'fa fa-laptop'} />
+          <HeaderBlock />
+        </article>
+        <article className="philosophy">
+          <SpinnyIcon icon={'fa fa-laptop'} />
+          <HeaderBlock />
+        </article>
+        <article className="philosophy">
+          <SpinnyIcon icon={'fa fa-laptop'} />
+          <HeaderBlock />
+        </article>
+      </aside>
     </div>
   )
 }
